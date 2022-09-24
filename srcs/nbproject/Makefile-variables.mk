cd0cf5d7475b582dc2ec223a4869e34e1cec58be
fdeĉ <fd@42l.fr>
2022-09-24 15:58:07 +0200

    update project to new srcs folder

--- srcs/nbproject/Makefile-variables.mk
+++ srcs/nbproject/Makefile-variables.mk
@@ -6,5 +6,5 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=robot.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/robot.production.hex
+CND_ARTIFACT_NAME_default=srcs.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/srcs.production.hex
