28dd4aed1d7a817140a2919a63bd106e014d5a20
fdeĉ <fd@42l.fr>
2022-09-24 16:51:38 +0200

    fix duty cycle

--- srcs/main.c
+++ srcs/main.c
@@ -10,24 +10,24 @@
 #pragma config PBADEN = OFF, WDT = OFF, LVP = OFF, DEBUG = ON
 
 void main(void) {
-    // Pin RC2 en sortie
+    // Pin RC2 as output
     TRISCbits.RC2 = 0;
-    // Pin RC1 en sortie
+    // Pin RC1 as ouput
     TRISCbits.RC1 = 0;
-    //Timer2 activation
+    // Activating Timer2
     T2CONbits.TMR2ON = 1;
 
     // Prescaler = /1
     T2CONbits.T2CKPS = 0;
     // PWM Period = 10 kHz => 4 000 000 / (4 * 10 000) - 1 = 99
     PR2 = 99;
-    // Rapport cyclique = 50% => (50 × 4 000 000) ÷ (100 × 10 000) = 200
-    CCPR1L = 200;
-    CCP1CONbits.DC1B = 0;
-    CCPR2L = 200;
-    CCP2CONbits.DC2B = 0;
+    // Duty cycle = 50% => (50 * 4 000 000) / (100 * 10 000) = 200
+    CCPR1L = 200 >> 2;
+    CCP1CONbits.DC1B = 200 & 0b11;
+    CCPR2L = 200 >> 2;
+    CCP2CONbits.DC2B = 200 & 0b11;
 
-    // Mode de PWM
+    // CCP in pwm mode
     CCP1CONbits.CCP1M = 0b1100;
     CCP2CONbits.CCP2M = 0b1100;
 
